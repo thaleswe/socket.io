1a94282accd8e8b9853ebe74184e3db856ef8891
$.Wendel <thaleswndl@gmail.com>
2022-06-14 22:54:52 -0300

    websocket transport 02

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-
-//Allow Cross Domain Requests
-Server.set('transports', [ 'websocket' ]);
-
-app.get('/', (req, res) => {
-    return res.sendFile(__dirname + '/index.html');
-});
-
-io.on("connection", (socket) => {
-    console.log("a user connected")
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-      });
-});
-
-
-
-server.listen(process.env.PORT || 5501, () => {
+const server = require('http').createServer(app).listen(process.env.PORT || 5501, () => {
   console.log('listening on *:5501');
 });
+
+const io = require('socket.io').listen(server);
+
+
+//Allow Cross Domain Requests
+io.set('transports', [ 'websocket' ]);
+
+console.log("agr foi")
