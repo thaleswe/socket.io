4e356dd46db6ef9a2c10fe572192ea66bb0f082e
$.Wendel <thaleswndl@gmail.com>
2022-06-14 22:37:54 -0300

    go go

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+//Allow Cross Domain Requests
+io.set('transports', [ 'websocket' ]);
+
+app.get('/', (req, res) => {
+    return res.sendFile(__dirname + '/index.html');
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected")
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
+});
+
+
+
+server.listen(process.env.PORT || 5501, () => {
+  console.log('listening on *:5501');
+});
