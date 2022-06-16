9d3612477610fb104df1a830a5f20987cfbdd85e
$.Wendel <thaleswndl@gmail.com>
2022-06-16 14:11:34 -0300

    commented io.set

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 //Allow Cross Domain Requests
-io.set('transports', [ 'websocket' ]);
+// io.set('transports', [ 'websocket' ]);
 
 app.get('/', (req, res) => {
     return res.sendFile(__dirname + '/index.html');
