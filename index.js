1d5beed614cbaa31ea6cce98317ed6fcb49b0875
$.Wendel <thaleswndl@gmail.com>
2022-06-16 14:16:29 -0300

    Commented io.set

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app).listen(process.env.PORT || 5501, () => {
-  console.log('listening on *:5501');
-});
-
-const io = require('socket.io').listen(server);
-
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 //Allow Cross Domain Requests
-io.set('transports', [ 'websocket' ]);
+// io.set('transports', [ 'websocket' ]);
 
-console.log("agr foi")
+app.get('/', (req, res) => {
+    return res.sendFile(__dirname + '/index.html');
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected")
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
+});
+
+
+
+server.listen(process.env.PORT || 5501, () => {
+  console.log('listening on *:5501');
+});
