4db8dac0695deaad25676beca7dcedcaccc3d982
$.Wendel <thaleswndl@gmail.com>
2022-06-16 16:18:52 -0300

    added public folder and socket.io file

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const server = require('http').createServer(app).listen(process.env.PORT || 5501, () => {
-  console.log('listening on *:5501');
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', (req, res) => {
+    return res.sendFile(__dirname + '/index.html');
 });
 
-const io = require('socket.io').listen(server);
+io.on("connection", (socket) => {
+    console.log("a user connected")
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
+});
 
 
-//Allow Cross Domain Requests
-// io.set('transports', [ 'websocket' ]);
 
-console.log("agr foi")
+server.listen(process.env.PORT || 5501, () => {
+  console.log('listening on *:5501');
+});
